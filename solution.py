71082bb58d9dccdd8849a596427ad99c359ecf33
grungemalevich <118844391+grungemalevich@users.noreply.github.com>
2023-04-13 00:41:00 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,19 +1,11 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import ttest_1samp
+from scipy.stats import ttest_ind
 
 chat_id = 487727948 # Ваш chat ID, не меняйте название переменной
 
-def solution(data: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    # Выполняем односторонний t-тест
-    t_statistic, p_value = ttest_1samp(data, 300)
-    
-    # Проверяем нулевую гипотезу на заданном уровне значимости
-    alpha = 0.1
-    if p_value/2 < alpha and t_statistic < 0:
-        return True # Отклоняем нулевую гипотезу
-    else:
-        return False # Не отклоняем нулевую гипотезу
+def solution(x, y) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+    flag = True
+    if ttest_ind(x, y, equal_var=False, alternative="less").pvalue >= 0.01:
+        flag = False
+    return flag # Ваш ответ, True или Fals
